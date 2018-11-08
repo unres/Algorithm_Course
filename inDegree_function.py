3e01e26927e2356e47751c85cdcdcb7a4f475959
Timothy Bell <timothy.bell@ttu.edu>
2018-11-08 00:17:23 -0600

    Added the variants and one invariant

--- inDegree_function.py
+++ inDegree_function.py
@@ -4,16 +4,16 @@
 def inDegrees(G):
   indegree = {}
   #invarient = 
-  #varient = 
+  #varient = The number of unproccesed elements
   # sets all the values of each vertex to 0
   for vertex in G:
       indegree[vertex] = 0    
   #invarient = 
-  #varient = 
+  #varient = The number of unproccesed elements
   #increments the value by 1 for each occurance 
   for vertex in G:
-      #invarient = 
-      #varient = 
+      #invarient = indegree[edge] + 1
+      #varient = The number of unproccesed elements
       for edge in G[vertex]:
         indegree[edge] += 1
   return indegree
